516a0dd24c4a9a8bd46073b2576fd917569a67bd
briangator19 <bgaetz381@hotmail.com>
2025-11-06 10:18:33 -0400

    Create seed_sample_data.sql

---
+++ scripts/seed_sample_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO players (name, tour, nationality) VALUES ('Rory McIlroy', 'PGA', 'N. Ireland');
+INSERT INTO players (name, tour, nationality) VALUES ('Jon Rahm', 'PGA', 'Spain');
+
+INSERT INTO courses (name, venue, course_rating, slope_rating) VALUES ('Pebble Beach Golf Links', 'Pebble Beach CA', 74.2, 140);
+INSERT INTO courses (name, venue, course_rating, slope_rating) VALUES ('Augusta National', 'Augusta GA', 76.1, 148);
